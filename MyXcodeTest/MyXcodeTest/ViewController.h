7ffd1315ffe1757468a0fa990adbad1a838f7cf0
zhaohu <526403189@qq.com>
2017-08-24 16:42:39 +0800

    GitHub test

---
+++ MyXcodeTest/MyXcodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyXcodeTest
+//
+//  Created by huzi_0118 on 2017/8/24.
+//  Copyright © 2017年 huzi_0118. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyXcodeTest/MyXcodeTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyXcodeTest
+//
+//  Created by huzi_0118 on 2017/8/24.
+//  Copyright © 2017年 huzi_0118. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
